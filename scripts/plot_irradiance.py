2502944529ea8266f711f4c4c433c17bbd72bc58
Giovanni Lopez <giovannilopez9808@gmail.com>
2025-08-01 20:21:22 -0600

    update

--- scripts/plot_irradiance.py
+++ scripts/plot_irradiance.py
@@ -5,6 +5,7 @@ from pandas import (
     DataFrame,
     Timestamp,
     read_csv,
+    to_datetime,
 )
 
 
@@ -54,20 +55,53 @@ for name in dataset.index:
     radiation = radiation.resample(
         "1min"
     ).mean()
-    dates = get_dates(
+    ticks = get_dates(
         radiation,
+    )
+    dates = list(
+        to_datetime(
+            f"{tick} 15:00"
+        )
+        for tick in ticks
     )
     pyplot.subplots(
         figsize=(
-            18,
-            10,
+            16,
+            5,
         )
     )
+    pyplot.ylim(
+        0,
+        0.6,
+    )
     pyplot.plot(
-        radiation,
+        radiation["zn"],
+        label="Zn",
+    )
+    pyplot.plot(
+        radiation["cu"],
+        label="Cu",
     )
     pyplot.xticks(
         dates,
+        ticks,
     )
-    pyplot.show()
-    exit(1)
+    pyplot.tick_params(
+        labelsize=13,
+    )
+    pyplot.legend(
+        fontsize=16,
+    )
+    folder = join(
+        "..",
+        "graphics",
+        "TUV"
+    )
+    filename = f"{name}.png"
+    filename = join(
+        folder,
+        filename,
+    )
+    pyplot.savefig(
+        filename,
+    )
